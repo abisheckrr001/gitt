446583aac06249949d6875ee0560d6205561504a
abisheckrr@gmail.com <abisheckrr@gmail.com>
2025-06-19 15:55:14 +0530

    added

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = 20;
+let b = 21.8;
+let c = "abisheck"
+let d = true;
+let e = 123456788;
+console.log(a);
+a = 10;
+ b = 10;
+ let f = a+b;
+ let g = a-b;
+ let h = a*b;
+ let i = a/b;
+ let j = a**b;
+ console.log(f!=g);
+ console.log(f||g);
+ console.log(a==b);
+
+ if (a==b) {
+    console.log("a and b are equal");
+ }   
+else {
+    console.log("a is not equal to b");
+}
+ 
+
+
