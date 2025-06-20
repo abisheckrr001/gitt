d57b639fd4bae42c095d4d1749bfd72117c31a29
abisheckrr@gmail.com <abisheckrr@gmail.com>
2025-06-20 16:19:43 +0530

    added

--- index.js
+++ index.js
@@ -23,4 +23,66 @@ else {
 }
  
 
+if(a%2==0){
+    console.log("even");
 
+}
+else{
+    console.log("odd");
+}
+
+
+
+for(let a=2 ; a<20; a++){
+    console.log(a);
+}
+
+
+
+
+let x=1;
+let y=2;
+function add(x,y){
+    return x+y;
+}
+console.log(add(x,y))
+d="hi!!"
+function name(d){
+    return d+" welcome";
+}
+console.log(name(d));
+
+let num = [1,2,3,4,5]
+
+
+let num1 = num.reduce((a1, b1) => a1+b1,0);
+console.log(num1);
+
+
+let mapp = num.map(a => a*a);
+console.log(mapp);
+
+
+let fil = num.filter(a => a%2 == 0);
+console.log(fil);
+
+
+let std1 = "vimal";
+let std2 = "vignesh";
+let std3 = "vishnu";
+let std4 = "venkatachlam";
+let std1_mark = [90,98,97,95,100];
+let std2_mark = [96,80,87,95,100];
+let std3_mark = [89,77,87,88,100];
+let std4_mark = [77,88,66,77,100];
+let std1t = std1_mark.reduce((a1, b1) => a1+b1,0);
+console.log("student 1 total : ",std1t);
+let std2t = std2_mark.reduce((a1, b1) => a1+b1,0);
+console.log("student 2 total : ",std2t);
+let std3t = std3_mark.reduce((a1, b1) => a1+b1,0);
+console.log("student 3 total : ",std3t);
+let std4t = std4_mark.reduce((a1, b1) => a1+b1,0);
+console.log("student 4 total : ",std4t);
+let arr = [std1t,std2t,std3t,std4t];
+let filt = arr.filter(arr => arr>450) ;
+console.log("mark more than 450 : ",filt);
